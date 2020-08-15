a768acc50f20d1d6112bf16dbd1c2feb5283cf3a
Adam Burgess <adamburgess@users.noreply.github.com>
2020-08-16 07:03:27 +1000

    add attribution

---
+++ README.md
@@ -0,0 +1,5 @@
+# Escape from Tarkov: Real time to In-game time
+
+## https://tarkov-time.netlify.app/
+
+Built with Preact + Tailwind.
--- src/App.tsx
+++ src/App.tsx
@@ -13,8 +13,12 @@ export function App() {
         <h1 class="text-3xl ff-heading font-black mt-3 text-center">Escape from Tarkov</h1>
         <h2 class="text-2xl xl:text-3xl ff-heading font-black text-center mb-2">Real time to In-game time</h2>
         <div class="grid grid-cols-2 grid-rows-1">
-            <Scroller side="left" time={time}/>
-            <Scroller side="right" time={time}/>
+            <Scroller side="left" time={time} />
+            <Scroller side="right" time={time} />
+        </div>
+        <div class="z-50 relative text-xs text-gray-500 text-right mt-2">
+            <div>Time in Escape from Tarkov moves at 7 seconds per second.</div>
+            <a href="https://github.com/adamburgess/tarkov-time">Source code at github.com/adamburgess/tarkov-time</a>
         </div>
     </div>
 }
