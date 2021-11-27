f3040d3e2ae3ed2486635eedc169c45f7b2b76b2
Adam Burgess <adamburgess@users.noreply.github.com>
2021-11-28 07:15:14 +1100

    add real responses

--- src/api.ts
+++ src/api.ts
@@ -1,10 +1,11 @@
 import { realTimeToTarkovTime } from './time'
 import { formatHMS } from './utils';
 
+interface VercelRequest {
+    url: string
+}
 type Handler = (event: {
-    request: {
-        url: string
-    }
+    request: VercelRequest
 }) => { response: Response, waitUntil: Promise<unknown> };
 
 declare let _ENTRIES: Record<string, { default: Handler }>;
@@ -14,11 +15,39 @@ const resolvedPromise = Promise.resolve();
 _ENTRIES = {
     "middleware_api": {
         default: ({ request }) => {
-            const time = formatHMS(realTimeToTarkovTime(new Date(), true));
             return {
-                response: new Response(time),
+                response: handle(request),
                 waitUntil: resolvedPromise
             };
         }
     }
+}
+
+function get(left: boolean) {
+    return formatHMS(realTimeToTarkovTime(new Date(), left));
+}
+
+function headers(extraHeaders: Record<string, string> = {}) {
+    return {
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Cache-Control': 'no-cache',
+            ...extraHeaders
+        }
+    }
+}
+
+function handle(request: VercelRequest): Response {
+    const url = new URL(request.url);
+    const path = url.pathname;
+
+    if (path === 'left') return new Response(get(true), headers());
+    else if (path === 'right') return new Response(get(false), headers());
+
+    return new Response(JSON.stringify({
+        left: get(true),
+        right: get(false),
+    }, null, 2), headers({
+        'Content-Type': 'application/json'
+    }));
 }
