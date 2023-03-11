0e078c581f0366408fde32dc301d62d4c4efc13f
Adam Burgess <adamburgess@users.noreply.github.com>
2023-03-11 22:39:52 +1100

    use getElementById. this actually compresses better

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,4 @@ import { h, render } from 'preact';
 import './tailwind.css'
 import { App } from './App';
 
-render(<App />, (document.all as unknown as { root: Element }).root!);
+render(<App />, document.getElementById('root')!);
